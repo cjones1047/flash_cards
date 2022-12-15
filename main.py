9dfcba36c8e63e21229832509211c11358abc096
cjones1047 <cjmj1047@gmail.com>
2022-12-15 06:45:47 -0600

    Images placed in correct locations for GUI

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flash Cards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+card = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_back_img = PhotoImage(file="./images/card_back.png")
+card.create_image(400, 263, image=card_front_img)
+card.grid(column=0, row=0, columnspan=2)
+
+correct_img = PhotoImage(file="./images/right.png")
+correct_button = Button(image=correct_img, )
+correct_button.grid(column=0, row=1)
+
+incorrect_img = PhotoImage(file="./images/wrong.png")
+incorrect_button = Button(image=incorrect_img)
+incorrect_button.grid(column=1, row=1)
+
+window.mainloop()
