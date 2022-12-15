fad71c0c17735c78ac9ed11fd9ccb696039a0fde
cjones1047 <cjmj1047@gmail.com>
2022-12-15 11:21:26 -0600

    Added text to card via create_text method of Canvas class

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-card = Canvas(width=800, height=526)
+card = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="./images/card_front.png")
 card_back_img = PhotoImage(file="./images/card_back.png")
 card.create_image(400, 263, image=card_front_img)
+card.create_text(400, 150, text="French", font=("Arial", 40, "italic"))
+card.create_text(400, 263, text="trouve", font=("Arial", 60, "bold"))
 card.grid(column=0, row=0, columnspan=2)
 
 correct_img = PhotoImage(file="./images/right.png")
-correct_button = Button(image=correct_img, )
+correct_button = Button(image=correct_img, highlightbackground=BACKGROUND_COLOR)
 correct_button.grid(column=0, row=1)
 
 incorrect_img = PhotoImage(file="./images/wrong.png")
-incorrect_button = Button(image=incorrect_img)
+incorrect_button = Button(image=incorrect_img, highlightbackground=BACKGROUND_COLOR)
 incorrect_button.grid(column=1, row=1)
 
 window.mainloop()
