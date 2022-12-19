7d6b6a570a9e822adcbbd3f01ad700475ecc740d
cjones1047 <cjmj1047@gmail.com>
2022-12-18 21:02:40 -0600

    Entire row of chosen word deleted from word_df dataframe if user clicks correct button; user will not be asked words they already answered correctly.

--- main.py
+++ main.py
@@ -7,23 +7,20 @@ STARTING_SECONDS = 5
 timer = ""
 
 
-def flip_card():
-    # get English translation of shown French word
+def get_french_word_row():
     french_word = card.itemcget(word, "text")
     word_row = word_df.loc[word_df["French"] == french_word]
+    return word_row
+
+
+def flip_card():
+    # get English translation of shown French word
+    word_row = get_french_word_row()
     english_word = word_row["English"].item()
     # flip card over
     card.itemconfig(language, text="English", fill="white")
     card.itemconfig(word, text=english_word, fill="white")
     card.itemconfig(card_side, image=card_back_img)
-
-
-def countdown(seconds_left):
-    if seconds_left > 0:
-        global timer
-        timer = window.after(1000, countdown, seconds_left - 1)
-    else:
-        flip_card()
 
 
 def pick_word():
@@ -34,7 +31,7 @@ def pick_word():
     except ValueError:
         pass
     # pick new word for card
-    word_picked = choice(word_dict)["French"]
+    word_picked = word_df.sample()["French"].item()
     card.itemconfig(language, text="French", fill="black")
     card.itemconfig(word, text=word_picked, fill="black")
     card.itemconfig(card_side, image=card_front_img)
@@ -42,12 +39,23 @@ def pick_word():
     timer = window.after(STARTING_SECONDS * 1000, flip_card)
 
 
-def remove_prev_word():
-    pass
+def remove_word():
+    # delete row of previous word from df if the user answered correct
+    # consider what translation of word need be searched based on what side of the card is showing
+    global word_df
+    try:
+        word_row_idx = get_french_word_row().index[0]
+    except IndexError:
+        english_word = card.itemcget(word, "text")
+        word_row = word_df.loc[word_df["English"] == english_word]
+        word_row_idx = word_row.index[0]
+    print(word_row_idx)
+    word_df = word_df.drop(word_row_idx)
+    print(word_df)
+    pick_word()
 
 
 word_df = pd.read_csv("./data/french_words.csv")
-word_dict = word_df.to_dict(orient="records")
 
 window = Tk()
 window.title("Flash Cards")
@@ -62,7 +70,7 @@ word = card.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 card.grid(column=0, row=0, columnspan=2)
 
 correct_img = PhotoImage(file="./images/right.png")
-correct_button = Button(image=correct_img, highlightbackground=BACKGROUND_COLOR, command=remove_prev_word)
+correct_button = Button(image=correct_img, highlightbackground=BACKGROUND_COLOR, command=remove_word)
 correct_button.grid(column=0, row=1)
 
 incorrect_img = PhotoImage(file="./images/wrong.png")
